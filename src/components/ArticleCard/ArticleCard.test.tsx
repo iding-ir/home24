e01b1c556cd9ede250397bd1414c4831efeffcbb
Aydin Ghane Kh <aydin.ghane.kh@gmail.com>
2021-12-25 20:08:22 +0330

    Improve tests

--- src/components/ArticleCard/ArticleCard.test.tsx
+++ src/components/ArticleCard/ArticleCard.test.tsx
@@ -7,26 +7,28 @@ jest.mock("react-i18next", () => ({
   useTranslation: () => ({ t: (key: string) => key }),
 }));
 
-test("renders the ArticleCard", () => {
-  const article = {
-    name: "Freischwinger Holm (2er-Set)",
-    variantName: "Buche",
-    prices: {
-      currency: "EUR",
-      regular: {
-        value: 24999,
-      },
+const article = {
+  name: "Freischwinger Holm (2er-Set)",
+  variantName: "Buche",
+  prices: {
+    currency: "EUR",
+    regular: {
+      value: 24999,
     },
-    images: [
-      {
-        path: "https://cdn1.home24.net/images/media/catalog/product/200x200/jpg/-/1/-1000059372-10-IMAGE-P000000001000059372.webp",
-      },
-    ],
-  } as Article;
+  },
+  images: [
+    {
+      path: "https://cdn1.home24.net/images/media/catalog/product/200x200/jpg/-/1/-1000059372-10-IMAGE-P000000001000059372.webp",
+    },
+  ],
+} as Article;
 
+test("renders the ArticleCard", () => {
   const { getByTestId } = render(<ArticleCard article={article} />);
 
   const articleCardElement = getByTestId("CARD");
 
   expect(articleCardElement).toBeInTheDocument();
+  expect(articleCardElement).toHaveTextContent("Freischwinger Holm (2er-Set)");
+  expect(articleCardElement).toHaveTextContent("249,99 €");
 });
